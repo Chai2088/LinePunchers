6e6ffafcbe618a130fa3891768be8753ed6707ca
SuyayC <suyay.charles@digipen.edu>
2024-01-27 19:33:37 +0100

    main menu

---
+++ My project (1)/Assets/Scripts/Button.cs
@@ -0,0 +1,34 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+  {
+ 
+    private Vector3 size;
+    void Start()
+    {
+        size = transform.localScale;
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    { 
+        transform.localScale = size * 1.1f;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = size;
+    }
+
+
+
+    public void OnButtonClick()
+    {
+        //if (GUI.Button(new Rect(10, 10, 100, 30), "Change Scene"))
+        { 
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+  }
