e9f93b803faf13d8639fc3a43c5ce65219f26656
Chai <98757417+Chai2088@users.noreply.github.com>
2024-01-27 23:19:43 +0100

    Some vfx

--- My project (1)/Assets/Scripts/CatLogic.cs
+++ My project (1)/Assets/Scripts/CatLogic.cs
@@ -9,11 +9,16 @@ public class CatLogic : MonoBehaviour
 
     public Animator anim;
     public float attackDist = 5.0f;
+
+    public ParticleSystem particle;
+    bool animationPlayed = false;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        particle = gameObject.GetComponent<ParticleSystem>();
         catAttack.SetActive(false);
+        particle.Stop();
     }
 
     // Update is called once per frame
@@ -21,9 +26,27 @@ public class CatLogic : MonoBehaviour
     {
         float distance = Vector3.Distance(transform.position, player.transform.position);
         AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
-        if(stateInfo.IsName("Armature|Attack01_001") && stateInfo.normalizedTime > 0.3f)
+        if(stateInfo.IsName("Armature|Attack01_001"))
         {
-            catAttack.SetActive(true);
+            if(stateInfo.normalizedTime > 0.3f)
+            {
+                catAttack.SetActive(true);
+                if(!animationPlayed)
+                {
+                    particle.Play();
+                    animationPlayed = true;
+                }
+            }
+            if(stateInfo.normalizedTime > 0.6f)
+            {
+                catAttack.SetActive(false);
+            }
+
+
+        }
+        else
+        {
+            animationPlayed = false;
         }
         if(!stateInfo.IsName("Armature|Parryhit01"))
         {
